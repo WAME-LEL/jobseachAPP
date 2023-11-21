bc8307f250de352c43ee26236d30c1885cfb56de
lel <fnskh0606@gmail.com>
2023-11-21 15:31:20 +0900

    cors policy resolve

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -10,9 +10,12 @@ import {
 } from 'react-native'
 
 import useFetch from '../../hook/useFetch'
+import {COLORS, icons} from "../../constants"
+import {ScreenHeaderBtn} from "../../components"
 
 const JobDetails = () => {
-    const params = useLocalSearchParams()
+    const router = useRouter()
+    const params = useLocalSearchParams()       //id가 여기에 들어감
     const {data, isLoading, error, refetch} = useFetch("job-details",{
         job_id:params.id
     })
@@ -25,7 +28,35 @@ const JobDetails = () => {
     },[])
 
     return (
-        <Text>{params.id}</Text>
+        <SafeAreaView style = {{flex:1, backgroundColor:COLORS.lightWhite}}>
+            {/* 공통된 레이아웃을 넣기위해 Stack.Screen 사용 */}
+            <Stack.Screen   
+                options={{
+                    headerStyle:{backgroundColor:COLORS.lightWhite},
+                    headerShadowVisible:false,
+                    headerBackVisible:false,
+                    headerLeft:() => (
+                    <ScreenHeaderBtn
+                        iconUrl={icons.left}
+                        dimension="60%"
+                        handlePress={()=>router.back()}
+                    />),
+                    headerRight:() => (
+                        <ScreenHeaderBtn
+                        iconUrl={icons.share}
+                        dimension="60%"
+                    />),
+                    
+                    
+                }}
+            
+            />
+            <ScrollView showsVerticalScrollIndicator = {false}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+            >
+
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
